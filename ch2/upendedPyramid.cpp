58edbeb44535a87d080ea5c35cc3f79429d7b05d
Patrick Rummage <patrickbrummage@gmail.com>
2014-11-14 21:53:48 -0500

    added pyramid exercise

---
+++ ch2/upendedPyramid.cpp
@@ -0,0 +1,36 @@
+/*
+* Upsidedown_pyramid.cpp
+* 
+* Prints out an upside down pyramid shape in hashes
+* 
+* 	########
+* 	 ######
+* 	  ####
+* 	   ##
+*/
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main(int argc, char *argv[])
+{
+	char outputChar;
+	for (int row = 1, spaces = 0; row <= 4; row++, spaces++)
+	{
+		for (int position = 1; position <= 8 - spaces; position++)
+		{
+		    if (position > spaces)
+		    {
+		    	outputChar = '#';
+		    }
+		    else outputChar = ' ';
+
+			cout << outputChar;
+		}
+		cout << "\n";
+	}
+	cout << "Press any key to exit: ";
+	cin.get();
+
+	return 0;
+}
