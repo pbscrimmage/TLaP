dcccdc70ae7a3814a54d5da9ac73596225662b9b
Patrick Rummage <patrickbrummage@gmail.com>
2014-11-14 20:46:25 -0500

    added ch1 folder

---
+++ ch1/isbn_checksum.cpp
@@ -0,0 +1,49 @@
+/**
+* isbn_checksum.cpp
+*
+* Author: Patrick Rummage (patrickbrummage@gmail.com)
+*
+* Calculates checksum to validate a 13 digit ISBN number.
+*/
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main(int argc, char *argv[])
+{
+	char digit;
+	int checksum = 0;
+	int digitCount = 1;
+
+	cout << "Enter number to validate: ";
+	digit = cin.get();
+
+	//process digits until ENTER (ASCII 10)
+	while (digit != 10)
+	{
+		if (digitCount % 2 == 0)
+		{
+			checksum += (digit - '0') * 3;
+		}
+		else
+		{
+			checksum += (digit - '0');
+		}
+		digit = cin.get();
+		digitCount++;
+	}
+
+	//validate checksum
+	cout << "Checksum is: " << checksum << ".\n";
+	if (checksum % 10 == 0)
+	{
+		cout << "VALID. Checksum is a multiple of 10.";
+	}
+	else
+	{
+		cout << "INVALID. Checksum is not a multiple of 10.";
+	}
+	cout << "Press any key to exit";
+	cin.get();
+	return 0;
+}
