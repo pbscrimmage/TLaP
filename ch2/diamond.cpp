37274a5ed8fedd7e9e2cfd66bc9005836f0f7666
Patrick Rummage <patrickbrummage@gmail.com>
2014-11-14 21:57:09 -0500

    added diamond exercise

---
+++ ch2/diamond.cpp
@@ -0,0 +1,40 @@
+/**
+* diamond.cpp
+*
+* Prints out a diamond shape in hashes
+*
+* 	    ##
+* 	   ####
+* 	  ######
+* 	 ########
+* 	 ########
+* 	  ######
+* 	   ####
+* 	    ##
+*/
+#include <iostream>
+#include <cstdlib>
+using std::cin;
+using std::cout;
+
+int main(int argc, char *argv[])
+{
+	char outputChar;
+	for (int row = 1, spaces = 3; row <= 8; row++, spaces = abs(4 - row))
+	{
+		for (int position = 1; position <= 8 - spaces; position++)
+		{
+		    if (position > spaces)
+		    {
+		    	outputChar = '#';
+		    }
+		    else outputChar = ' ';
+
+			cout << outputChar;
+		}
+		cout << "\n";
+	}
+	cout << "Press any key to exit: ";
+	cin.get();
+	return 0;
+}
