afb8fdcf89a5bd8c81b44548e004482314d009aa
Patrick Rummage <patrickbrummage@gmail.com>
2014-12-04 03:17:20 -0500

    K&R brackets, replaceString partially working

--- ch4_pointers_dynamic_mem/lengthPrefixStrings.cpp
+++ ch4_pointers_dynamic_mem/lengthPrefixStrings.cpp
@@ -33,12 +33,10 @@ void concatenate(lengthString &s1, lengthString s2)
     int newLength = s1_length + s2_length;
     lengthString newS = new char[newLength + 1];
     newS[0] = newLength;
-    for (int i = 1; i <= s1_length; i++)
-    {
+    for (int i = 1; i <= s1_length; i++) {
         newS[i] = s1[i];
     }
-    for (int i = 1; i <= s2_length; i++)
-    {
+    for (int i = 1; i <= s2_length; i++) {
         newS[s1_length + i] = s2[i];
     }
     delete[] s1;
@@ -48,10 +46,8 @@ void concatenate(lengthString &s1, lengthString s2)
 bool stringEqual(lengthString s1, lengthString s2)
 {
     int s_length = s1[0];
-    for (int i = 1; i <= s_length; i++)
-    {
-        if (s1[i] != s2[i])
-        {
+    for (int i = 1; i <= s_length; i++) {
+        if (s1[i] != s2[i]) {
             return false;
         }
     }
@@ -62,45 +58,41 @@ lengthString substring(lengthString s, int position, int length)
 {
     lengthString sub = new char[length + 1];
     sub[0] = length;
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         sub[1 + i] = s[position + i];
     }
     return sub;
 }
-//BROKEN!!
+/*working for single, same-size replacements*/
 void replaceString(lengthString &s, lengthString target, lengthString replaceText)
 {
     int s_length = s[0];
     int targetLength = target[0];
     int replaceLength = replaceText[0];
-    int newLength = s_length + (replaceLength - targetLength);
-    lengthString newS = new char[newLength]; //FIX: DO WITHIN LOOP
-    bool match = false;
-    for (int i = 1; i <= s_length; i++)
-    {
-        if (s[i] == target[1])
-        {
-            if (stringEqual(substring(s, i, targetLength), target))
-            {
-                concatenate(newS, substring(s, 1, i - 1));
-                concatenate(newS, replaceText);
-                concatenate(newS, substring(s, i + targetLength, s_length - (i + 1)));
-            }
+    for (int i = 1; i <= s_length; i++) {
+        if (s[i] == target[1] && 
+                stringEqual(substring(s, i, targetLength), target)) {
+ 
+            int currentLength = s[0]; //In case length has changed
+            int newLength = currentLength + (replaceLength - targetLength);
+            /*Allocate and create new string*/
+            lengthString newS = new char[newLength];
+            lengthString begin = substring(s, 1, i - 1);
+            lengthString end = substring(s, i + targetLength, s_length - (i + 1));
+            concatenate(newS, begin); //Original beginning
+            concatenate(newS, replaceText); //Replace stuff
+            concatenate(newS, end); //Original end
+            delete[] s;
+            s = newS;
         }
     }
-    if (!match)
-        concatenate(newS, s);
-    delete[] s;
-    s = newS;
 }
 
 void input(lengthString &s)
 {
 
     char inputChar = cin.get();
-    while (inputChar != 10)
-    {
+    while (inputChar != 10) {
         append(s, inputChar);
         inputChar = cin.get(); 
     }
@@ -108,8 +100,7 @@ void input(lengthString &s)
 void output(lengthString s)
 {
     int length = s[0];
-    for(int i = 1; i <= length; i++)
-    {
+    for(int i = 1; i <= length; i++) {
         cout << s[i];
     }
     cout << "\n";
