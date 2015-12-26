0d74f1cb89da169552f375a156f9461c6f81d387
pbscrimmage <patrickbrummage@gmail.com>
2015-12-26 13:36:43 -0500

    tentative node adding

--- ch6_recursion/binaryTree_class.cpp
+++ ch6_recursion/binaryTree_class.cpp
@@ -5,13 +5,18 @@
  *
  */
 #include <iostream>
+#include <vector>
 using std::cin;
 using std::cout;
+using std::vector;
 
 class binaryTree {
     public:
         binaryTree();
+        binaryTree(vector<int> numbers);
         ~binaryTree();
+        int countNodes();
+        void addNode(int n); // Public wrapper for insertNode
         bool isSearchTree();
         bool isHeap();
         void makeSearchTree();
@@ -23,6 +28,7 @@ class binaryTree {
             treeNode* right;
         };
         treeNode* _root;
+        void insertNode(int n, treeNode* treePtr);
 };
 
 //-----------------------------------------------------------------------------
@@ -30,7 +36,35 @@ binaryTree::binaryTree() {
     _root = NULL;
 }
 
+//-----------------------------------------------------------------------------
+binaryTree::binaryTree(vector<int> numbers) {
+    for (int n : numbers) {
+        insertNode(n, _root);
+    }
+}
+
+//-----------------------------------------------------------------------------
+void binaryTree::insertNode(int n, treeNode* treePtr) {
+    if (treePtr == NULL) {
+        treePtr = new treeNode{n,NULL,NULL};
+    } else {
+        if (n <= treePtr->data) {
+            insertNode(n, treePtr->left);
+        } else {
+            insertNode(n, treePtr->right);
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------
+binaryTree::~binaryTree() {
+
+}
+
 int main(int argc, char* argv[]) {
     
+    vector<int> testvals{0,1,2,3,4,5,6,7,8,9,10};
+    binaryTree bt{testvals};
+
     return 0;
 }
